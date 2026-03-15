c41ee719905212c597f93ebe1538df1e798101cf
dorireuv <dori.public@gmail.com>
2026-03-16 00:39:43 +0200

    TextFieldPageObject - add custom text field support.

--- lib/src/text_field_page_object.dart
+++ lib/src/text_field_page_object.dart
@@ -2,12 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'page_object.dart';
+import 'page_object_builder.dart';
 import 'page_object_factory.dart';
 
 /// A page object representing a [TextField] widget.
-class TextFieldPageObject extends PageObject {
-  /// Creates a [TextFieldPageObject] with the given [finder].
-  TextFieldPageObject(super.t, super.finder);
+class TextFieldPageObject<T extends Object> extends PageObject {
+  /// Creates a [TextFieldPageObject] with the given [finder], for text
+  /// fields with values of type [T], using the given [formatter] and [parser].
+  static PageObjectBuilder<TextFieldPageObject<T>> builder<T extends Object>({
+    required String Function(T v) formatter,
+    required T? Function(String v) parser,
+  }) =>
+      (t, finder) =>
+          TextFieldPageObject(t, finder, formatter: formatter, parser: parser);
+
+  final String Function(T v) _formatter;
+  final T? Function(String v) _parser;
+
+  /// Creates a [TextFieldPageObject] with the given [finder], [formatter],
+  /// and [parser]
+  TextFieldPageObject(
+    super.t,
+    super.finder, {
+    required String Function(T v) formatter,
+    required T? Function(String v) parser,
+  })  : _formatter = formatter,
+        _parser = parser;
 
   /// Enters the given text into the text field.
   Future<void> setText(String v) => t.enterText(this, v);
@@ -23,10 +43,31 @@ class TextFieldPageObject extends PageObject {
         find.descendant(of: this, matching: find.byType(EditableText)));
     return editableText.controller.text;
   }
+
+  /// Sets the value of the text field to the given value of type [T], by
+  /// formatting it to a string and entering the text.
+  Future<void> set(T v) => setText(_formatter(v));
+
+  /// Gets the current value of the text field as type [T], by parsing the
+  /// current text value. Returns null if the text value cannot be parsed to a
+  /// value of type [T].
+  T? get value => _parser(textValue);
 }
 
 /// Extension on [PageObjectFactory] to create [TextFieldPageObject]s.
 extension TextFieldPageObjectFactoryExtension<K> on PageObjectFactory<K> {
   /// Creates a [TextFieldPageObject] with the given [key].
-  TextFieldPageObject textField(K key) => create(TextFieldPageObject.new, key);
+  TextFieldPageObject<String> textField(K key) =>
+      customTextField(key, formatter: (v) => v, parser: (v) => v);
+
+  /// Creates a [TextFieldPageObject] with the given [key], for text
+  /// fields with values of type [T], using the given [formatter] and [parser].
+  TextFieldPageObject<T> customTextField<T extends Object>(
+    K key, {
+    required String Function(T v) formatter,
+    required T? Function(String v) parser,
+  }) =>
+      create(
+          TextFieldPageObject.builder<T>(formatter: formatter, parser: parser),
+          key);
 }
--- test/text_field_page_object_test.dart
+++ test/text_field_page_object_test.dart
@@ -5,26 +5,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'common.dart';
 
 void main() {
-  TextFieldPageObject createPageObject(WidgetTester t) =>
+  TextFieldPageObject<String> createStringPageObject(WidgetTester t) =>
       PageObjectFactory.root(t).textField(aFinder);
+
+  TextFieldPageObject<bool> createBoolPageObject(WidgetTester t) =>
+      PageObjectFactory.root(t).customTextField(
+        aFinder,
+        formatter: (v) => v.toString(),
+        parser: bool.tryParse,
+      );
 
   group('textValue', () {
     testWidgets('not set without controller --> empty', (t) async {
       await t.pumpWidget(const _Widget());
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
       expect(pageObject.textValue, '');
     });
 
     testWidgets('not set with controller --> controller value', (t) async {
       await t.pumpWidget(
           _Widget(controller: TextEditingController(text: 'initial')));
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
       expect(pageObject.textValue, 'initial');
     });
 
     testWidgets('setText without controller', (t) async {
       await t.pumpWidget(const _Widget());
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
       await pageObject.setText('text');
 
@@ -33,11 +40,40 @@ void main() {
 
     testWidgets('setText with controller', (t) async {
       await t.pumpWidget(_Widget(controller: TextEditingController()));
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
       await pageObject.setText('text');
 
       expect(pageObject.textValue, 'text');
+    });
+  });
+
+  group('value', () {
+    testWidgets('set', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.set(true);
+
+      expect(pageObject.value, true);
+    });
+
+    testWidgets('setText with valid value --> value', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.setText(true.toString());
+
+      expect(pageObject.value, true);
+    });
+
+    testWidgets('setText with invalid value --> null', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.setText('invalid');
+
+      expect(pageObject.value, null);
     });
   });
 }
