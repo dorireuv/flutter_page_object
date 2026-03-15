7cd097ae8925cdcca9342c8134bf4509795de5b0
dorireuv <dori.public@gmail.com>
2026-03-16 00:26:20 +0200

    TextFormFieldPageObject - re-add customTextFormField.

--- test/text_form_field_page_object_test.dart
+++ test/text_form_field_page_object_test.dart
@@ -5,40 +5,97 @@ import 'package:flutter_test/flutter_test.dart';
 import 'common.dart';
 
 void main() {
-  TextFormFieldPageObject createPageObject(WidgetTester t) =>
+  TextFormFieldPageObject<String> createStringPageObject(WidgetTester t) =>
       PageObjectFactory.root(t).textFormField(aFinder);
+
+  TextFormFieldPageObject<bool> createBoolPageObject(WidgetTester t) =>
+      PageObjectFactory.root(t).customTextFormField(
+        aFinder,
+        formatter: (v) => v.toString(),
+        parser: bool.tryParse,
+      );
 
   group('textValue', () {
     testWidgets('not set without initial value --> empty', (t) async {
       await t.pumpWidget(const _Widget());
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
       expect(pageObject.textValue, '');
     });
 
     testWidgets('not set with initial value --> initial value', (t) async {
       await t.pumpWidget(const _Widget(initialValue: 'initialValue'));
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
       expect(pageObject.textValue, 'initialValue');
     });
 
-    testWidgets('setText without controller', (t) async {
+    testWidgets('set without controller', (t) async {
       await t.pumpWidget(const _Widget());
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
-      await pageObject.setText('text');
+      await pageObject.set('text');
 
       expect(pageObject.textValue, 'text');
     });
 
-    testWidgets('setText with controller', (t) async {
+    testWidgets('set with controller', (t) async {
       await t.pumpWidget(_Widget(controller: TextEditingController()));
-      final pageObject = createPageObject(t);
+      final pageObject = createStringPageObject(t);
 
-      await pageObject.setText('text');
+      await pageObject.set('text');
 
       expect(pageObject.textValue, 'text');
+    });
+  });
+
+  group('value', () {
+    testWidgets('without initial value --> null', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      expect(pageObject.value, null);
+    });
+
+    testWidgets('with valid initial value --> initial value', (t) async {
+      await t.pumpWidget(_Widget(initialValue: true.toString()));
+      final pageObject = createBoolPageObject(t);
+
+      expect(pageObject.value, true);
+    });
+
+    testWidgets('with invalid initial value --> null', (t) async {
+      await t.pumpWidget(const _Widget(initialValue: 'invalid'));
+      final pageObject = createBoolPageObject(t);
+
+      expect(pageObject.value, null);
+    });
+
+    testWidgets('set', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.set(true);
+
+      expect(pageObject.value, true);
+    });
+
+    testWidgets('setText with valid value --> value', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.setText(true.toString());
+
+      expect(pageObject.value, true);
+    });
+
+    testWidgets('setText with invalid value --> null', (t) async {
+      await t.pumpWidget(const _Widget());
+      final pageObject = createBoolPageObject(t);
+
+      await pageObject.setText('invalid');
+
+      expect(pageObject.value, null);
     });
   });
 }
